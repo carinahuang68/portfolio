7a05cba5622e563e4a1af72c7ea3023ba41ebe55
Carina Huang <huangc24@students.cs.ubc.ca>
2025-07-25 15:56:55 -0700

    Update projects and enhance UI

--- numberGuessing.js
+++ numberGuessing.js
@@ -1,13 +1,14 @@
 let button = document.getElementById('button');
 let output = document.getElementById('outputtext');
 let guesses = document.getElementById('guesses');
+let userInput = document.getElementById('userInput');
 
 let number = Math.floor(Math.random()*100+1);
 let numOfGuess = 10;
 
 button.addEventListener('click',function(){
   numOfGuess--;
-  let input = document.getElementById('userInput').value;
+  let input = userInput.value;
   if(input==number){
     output.innerHTML = `<font color=\"green\">Bingo!</font> The number was ${number}.`;
   } else if(input < number){
@@ -16,8 +17,10 @@ button.addEventListener('click',function(){
     output.innerHTML = "<font color=\"red\">Too high!</font> Please try again."
   } else{
     alert('Please enter a number')
+    numOfGuess++;
   }
   guesses.innerHTML = `Remaining guesses: <strong>${numOfGuess}</strong>`;
+  userInput.value = "";
   if(numOfGuess <= 0 && input!=number){
     output.innerHTML = "Game Over!"
   }
