1cbd9e2e7628a80268724e6f72018b2dec19b93f
Carina Huang <huangc24@students.cs.ubc.ca>
2025-08-07 18:32:28 -0700

    Add navbar and home page

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-placeholder").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+});
